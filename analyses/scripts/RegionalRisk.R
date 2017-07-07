a45d3f2bf3c2634363cbd29c1a382ff89c01c89d
cchambe12 <cchamberlain@g.harvard.edu>
2017-07-07 11:57:38 -0400

    working on getting last observation

--- analyses/scripts/RegionalRisk.R
+++ analyses/scripts/RegionalRisk.R
@@ -14,6 +14,7 @@ library(ggplot2)
 library(lubridate)
 library(arm)
 library(data.table)
+library(plyr)
 
 
 setwd("~/Documents/git/regionalrisk/analyses/output")
@@ -37,66 +38,64 @@ clim$long<-round(clim$long, digits=2)
 d<-full_join(clim, bb)
 d<-filter(d, year>=1950)
 
-how_many<-d[which(is.na(d$Tmin)),]
+#how_many<-d[which(is.na(d$Tmin)),]
 d<-d[!(is.na(d$Tmin) & is.na(d$PEP_ID)),]
 
-df<-d%>%dplyr::select(year, PEP_ID, BBCH)
-df$BBCH<-ifelse(df$BBCH>=7, df$BBCH, NA)
-df<-df%>%group_by(year,PEP_ID)%>%arrange(year)
-df<-na.omit(df)
-df <- within(df, { count <- ave(BBCH,PEP_ID, year, FUN=function(x) length(unique(x)))})
-df$count<-ifelse(df$count>=2,df$count, NA)
-xx<-na.omit(df)
-unique(xx$year) ## 1997-2015
-
-peps<-unique(xx$PEP_ID)
-years<-c(1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015)
-
-frz<-d%>%filter(year%in%years)%>%filter(PEP_ID %in% peps)
-many<-frz[which(is.na(frz$Tmin)),]
-frz<-frz[!(is.na(frz$Tmin)),]
-
-frz$lat.long<-paste(frz$lat,frz$long, sep=",")
-
-##### Now find climate data ######
-d$lat.long<-paste(d$lat, d$long, sep=",")
-lats<-unique(frz$lat.long)
-dx<-filter(d, lat.long%in%lats)
-dx<-dx[!(is.na(dx$Tmin)),]
-dx$grow<-ifelse(is.na(dx$BBCH), NA, TRUE)
-dx$count <- ave(
-  dx$grow, dx$PEP_ID, dx$year,
+d$grow<-ifelse(is.na(d$BBCH), NA, TRUE)
+d$count <- ave(
+  d$grow, d$PEP_ID, d$year,
   FUN=function(x) cumsum(c(1, head(x, -1)))
 )
 
-dx$frz<- ifelse((dx$Tmin<=-2.2), 1, 0)
+d$frz<- ifelse((d$Tmin<=-2.2), 1, 0)
 
-
-climate<-clim%>%filter(year==1997)
-plz<- dx%>% filter(year==1997)
-plz<-na.omit(plz)
-peps<-unique(plz$PEP_ID[which(plz$count==2)])
-plz<-plz%>%filter(PEP_ID %in% peps)
+plz<- d#%>% filter(year==1997)
+plz<-plz[!(is.na(plz$count) & is.na(plz$Tmin)),]
+plz<- plz[ave(plz$year, plz$PEP_ID, FUN = length) > 1, ]
 plz$count<-ifelse(plz$count==1, "start", plz$count)
+
+############# WORK ON LAST OBSERVATION! ######################
+plz$pepyear<-paste(plz$PEP_ID, plz$year, sep=",")
+plz$pepBB<-paste(plz$pepyear, plz$BBCH, sep=",")
+test<-plz
+#test<- within(test, count[test$count==2]<-2)
+tt<-test%>%
+  filter(count==2) %>%
+  group_by(PEP_ID, year) %>%
+  filter(date==max(date))
+  
+tt<-within(tt, count[tt$count==2]<-"end")
+dat <- merge(plz, tt, by = "pepBB", all.x = TRUE)
+dat<-dat[!(dat$count.x=="start" & dat$count.y=="end"),]
+transform(dat, color = ifelse(plz$count!="end", 
+                              plz$count, tt$count))[-(2:3)]
+
+#############################################################
 plz$count<-ifelse(plz$count==2, "end", plz$count)
 plz$count<-ifelse(plz$count==3, NA, plz$count)
 plz<-na.omit(plz)
 
+plz$pepyear<-paste(plz$PEP_ID, plz$year, sep=",")
+ends<- unique(plz$pepyear[which(plz$count=="end")])
+  
+plz<-filter(plz, pepyear %in% ends)
 plz$start<-ifelse(plz$count=="start", plz$date, NA)
 class(plz$start)<-"Date"
 plz$end<-ifelse(plz$count=="end", plz$date, NA)
 class(plz$end)<-"Date"
   
-plzers<-plz%>%dplyr::select(year, PEP_ID, start, end, lat, long)
-plzers<-setDT(plzers)[, lapply(.SD, na.omit), by = PEP_ID]
+plzers<-plz%>%dplyr::select(year, PEP_ID, start, end, lat, long, pepyear)%>%group_by(PEP_ID, year)%>%arrange(PEP_ID)
+
+plzers<-setDT(plzers)[, lapply(.SD, na.omit), by = pepyear]
 plzers<-plzers[!duplicated(plzers), ]
+
 
 please<-plzers %>%
   arrange(PEP_ID)%>%
   rowwise()%>%
-  do(data.frame(PEP_ID = .$PEP_ID, lat= .$lat, long=.$long, date = seq(.$start, .$end, by = "days")))
+  do(data.frame(PEP_ID=.$PEP_ID, lat= .$lat, long=.$long, date = seq.Date(.$start, .$end, by=1)))
 
-freeze<-left_join(please,climate)
+freeze<-left_join(please,clim)
 
 freeze$frz<-ifelse(freeze$Tmin<=-2.2, 1, 0)
 freeze$count <- ave(
@@ -104,48 +103,10 @@ freeze$count <- ave(
   FUN=function(x) cumsum(c(0, head(x, -1)))
 )
 
-freeze<-ungroup(freeze)
-test<- freeze %>% group_by(PEP_ID, lat, long, year) %>% summarise(count = max(count))
+#freeze<-ungroup(freeze)
+#test<- freeze %>% group_by(PEP_ID, lat, long, year) %>% summarise(count = max(count))
 
-mod<-lm(lat~count+long, data=test)
-display(mod)
-
+ggplot((freeze), aes(x=long, y=lat)) + geom_point(aes(col=count))
 
 #write.csv(d, "~/Documents/git/regionalrisk/analyses/output/acer_combined.csv", row.names=FALSE)
-
-
-
-########################## Lame Attempts to keep for now #########################
-
-dxx<-dx%>%dplyr::select(PEP_ID, BBCH, lat.long, year, date)
-dxx<-dxx%>%
-  group_by(year, PEP_ID,BBCH)%>%
-  arrange(PEP_ID)%>%
-  filter(between(row_number(), 1, n()))
-dxx<-na.omit(dxx)
-dxx<-dplyr::select(dxx, -date)
-dxx$BBCH<-ifelse(is.na(dxx$BBCH), NA, TRUE)
-dxx<-data.frame(dxx,first=!duplicated(dxx),last=rev(!duplicated(rev(dxx))))
-dxx$one<-ifelse(dxx$first==TRUE & dxx$last==TRUE, FALSE, dxx$first)
-dxx$two<-ifelse(dxx$first==TRUE & dxx$last==TRUE, FALSE, dxx$last)
-dxx<-dxx%>%dplyr::select(-first, -last)
-dxx$BBCH<-ifelse(is.na(dxx$BBCH), NA, TRUE)
-dxx<-data.frame(dxx,first=!duplicated(dxx),last=rev(!duplicated(rev(dxx))))
-
-dx$obs<-ifelse(dx$BBCH<=14, start, dx$BBCH)
-dx$obs<-ifelse(dx$BBCH==15, end, dx$BBCH)
-
-
-dx<-dx%>%
-  group_by(year, PEP_ID,BBCH)%>%
-  arrange(PEP_ID)%>%
-  filter(between(row_number(), 1, n()))
-dx<-ungroup(dx)
-dx<-dx%>%group_by(date, lat.long)%>%arrange(date)
-dx$frz<- ifelse((dx$Tmin<=-2.2), 1, 0)
-dx$count <- ave(
-  dx$frz, dx$lat.long, dx$year,
-  FUN=function(x) cumsum(c(0, head(x, -1)))
-)
-
 
