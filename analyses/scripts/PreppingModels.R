d02d679728949d939142b7e5d05bcf2a8430911f
cchambe12 <cchamberlain@g.harvard.edu>
2018-01-17 11:11:12 -0500

    working on mat data

--- analyses/scripts/PreppingModels.R
+++ analyses/scripts/PreppingModels.R
@@ -12,10 +12,10 @@ graphics.off()
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-library(rstanarm)
-library(rstan)
-library(bayesplot)
-library(shinystan)
+#library(rstanarm)
+#library(rstan)
+#library(bayesplot)
+#library(shinystan)
 
 
 library(lubridate)
@@ -25,109 +25,22 @@ library(chillR)
 library(raster)
 library(reshape2)
 library(data.table)
+library(velox)
 
 ### Load data
 setwd("~/Documents/git/regionalrisk/analyses/output")
 d<-read.csv("fs_yearsitespp.csv", header=TRUE)
-
+dx<-read.csv("mat.csv", header=TRUE)
 
 ### Clean the weather data
 setwd("~/Desktop/")
 r<-brick("~/Desktop/tg_0.25deg_reg_v16.0.nc", varname="tg", sep="")
 
-## BETPEN
-bp<- d %>% filter(species=="BETPEN")
-bp$lat.long<-paste(bp$lat, bp$long, sep=",")
-bp<-bp[!duplicated(bp$lat.long),]
-lats <- bp$lat
-lons <- bp$long
-coords <- data.frame(x=lons,y=lats)
-
-points <- SpatialPoints(coords, proj4string = r@crs)
-
-values <- extract(r,points)
-
-bpclim <- cbind.data.frame(coordinates(points),values)
-
-bp<-melt(bpclim, id.vars=c("x","y"))
-
-bp<-bp%>%
-  rename(long=x)%>%
-  rename(lat=y)%>%
-  rename(date=variable)%>%
-  rename(Tavg=value)
-
-bp$date<-substr(bp$date, 2,11)
-bp$Date<-gsub("[.]","-", bp$date)
-
-bp<-dplyr::select(bp, -date)
-
-## AESHIP
-ah<- d %>% filter(species=="AESHIP")
-ah$lat.long<-paste(ah$lat, ah$long, sep=",")
-ah<-ah[!duplicated(ah$lat.long),]
-lats <- ah$lat
-lons <- ah$long
-coords <- data.frame(x=lons,y=lats)
-
-points <- SpatialPoints(coords, proj4string = r@crs)
-
-values <- extract(r,points)
-
-bpclim <- cbind.data.frame(coordinates(points),values)
-
-bp<-melt(bpclim, id.vars=c("x","y"))
-
-bp<-bp%>%
-  rename(long=x)%>%
-  rename(lat=y)%>%
-  rename(date=variable)%>%
-  rename(Tavg=value)
-
-bp$date<-substr(bp$date, 2,11)
-bp$Date<-gsub("[.]","-", bp$date)
-
-bp<-dplyr::select(bp, -date)
-
-## ALNGLU
-ag<- d %>% filter(species=="ALNGLU")
-ag$lat.long<-paste(ag$lat, ag$long, sep=",")
-ag<-ag[!duplicated(ag$lat.long),]
-lats <- ag$lat
-lons <- ag$long
-coords <- data.frame(x=lons,y=lats)
-
-points <- SpatialPoints(coords, proj4string = r@crs)
-
-values <- extract(r,points)
-
-agclim <- cbind.data.frame(coordinates(points),values)
-
-ag<-melt(agclim, id.vars=c("x","y"))
-
-ag<-ag%>%
-  rename(long=x)%>%
-  rename(lat=y)%>%
-  rename(date=variable)%>%
-  rename(Tavg=value)
-
-ag$date<-substr(ag$date, 2,11)
-ag$Date<-gsub("[.]","-", ag$date)
-
-ag<-dplyr::select(ag, -date)
-
-fe<- d %>% filter(species=="FRAEXC")
-fs<- d %>% filter(species=="FAGSYL")
-qr<- d %>% filter(species=="QUEROB")
-
-dx<-d
-dx$lat.long<-paste(dx$lat, dx$long, sep=",")
-dx<-dx[!duplicated(dx$lat.long),]
-lats <- dx$lat
-lons <- dx$long
-
-coords <- data.frame(x=lons,y=lats)
-
+d$lat.long<-paste(d$lat, d$long, sep=",")
+d<-d[!duplicated(d$lat.long),]
+lats<-d$lat
+lons<-d$long
+coords<-data.frame(x=lons, y=lats)
 points <- SpatialPoints(coords, proj4string = r@crs)
 
 values <- extract(r,points)
@@ -145,9 +58,14 @@ dx<-dx%>%
 dx$date<-substr(dx$date, 2,11)
 dx$Date<-gsub("[.]","-", dx$date)
 
-dxx<-dplyr::select(dxx, -date)
 dx<-dplyr::select(dx, -date)
 
+#write.csv(dx, file="~/Documents/git/regionalrisk/analyses/output/mat.csv", row.names=FALSE)
+
+dx$year<- substr(dx$Date, 7, 10)
+dx$mat<-ave(dx$Tavg, dx$year, dx$lat, dx$long)
+dx<-dplyr::select(dx, -Tavg)
+dx<-dx
 
 ## Start running models...
 d$PEP_ID<-as.numeric(as.factor(d$PEP_ID))
