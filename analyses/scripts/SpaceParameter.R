d95714f6c1070aa3b5946cb316d02c1bc8d70d83
cchambe12 <cchamberlain@g.harvard.edu>
2018-09-13 10:39:30 -0400

    space parameter update

--- analyses/scripts/SpaceParameter.R
+++ analyses/scripts/SpaceParameter.R
@@ -64,6 +64,27 @@ graph2nb <- function(gob, row.names=NULL,sym=FALSE) {
   res
 }
 
+gabrielneigh <- function(coords, nnmult=3) {
+  x <- coords
+  if (!is.matrix(x)) stop("Data not in matrix form")
+  if (any(is.na(x))) stop("Data cannot include NAs")
+  np <- nrow(x)
+  if(ncol(x)!=2) stop("Planar graphs only work in 2d")
+  ngaballoc <- np*nnmult
+  g1<-g2<-rep(0,ngaballoc)
+  nogab <- 0
+  storage.mode(x) <- "double"
+  z <- .C("compute_gabriel", np=as.integer(np), from=as.integer(g1),
+          to=as.integer(g2), nedges=as.integer(nogab), 
+          ngaballoc=as.integer(ngaballoc), x=x[,1], 
+          y=x[,2], PACKAGE="spdep")
+  z$from<-z$from[1:z$nedges]
+  z$to<-z$to[1:z$nedges]
+  attr(z, "call") <- match.call()
+  class(z)<-c("Graph","Gabriel")
+  z
+}
+
 #bc<-bcoords[sample(nrow(bcoords), 100, replace=FALSE),]
 nb<-graph2nb(gabrielneigh(as.matrix(bcoords)), sym=TRUE)
 
