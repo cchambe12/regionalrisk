612849023751bcff057a24b798d43aea7770b539
cchambe12 <cchamberlain@g.harvard.edu>
2019-02-01 11:10:39 -0500

    working on APC plots

--- analyses/scripts/models_plotting_APC.R
+++ analyses/scripts/models_plotting_APC.R
@@ -11,16 +11,35 @@ library(brms)
 library(ggplot2)
 library(egg)
 library(RColorBrewer)
-library(sjmisc)
-library(sjPlot)
-library(ggeffects)
+library(tidybayes)
 library(broom)
+library(dplyr)
 
 setwd("~/Documents/git/regionalrisk/")
 
-load("orig_full.Rdata")
+#load("orig_full.Rdata")
+
+varies <- get_variables(orig.full)
+varies <- gsub(".*b_","",varies)
+
+check <- bb %>% add_fitted_draws(orig.full, prob=0.5) %>%
+  filter(species==c("FRAEXC", "BETPEN"))
+
+foo <- ungroup(check) %>%
+  dplyr::select(species, cc, elev, .value)
+
+foo <- foo[!duplicated(foo),] ### super close!!! Need to find a way to get 50% cred intervals
+
+ggplot(foo, aes(x=elev, y=.value, col=species, linetype=as.factor(cc))) + geom_line(aes(col=species, linetype=as.factor(cc)), 
+                                                                                 stat="smooth", method="lm", se=TRUE, span=0.5)
+
+goober <- posterior_interval(orig.full, prob=0.5)
 
 sumer.orig<-as.data.frame(tidy(orig.full,robust = TRUE, prob=0.5))
 
+sumer.orig$term<-gsub(".*b_","",sumer.orig$term)
+
+sumer.orig<-sumer.orig[(sumer.orig$term=="mat.z:cc.z:speciesBETPEN" | sumer.orig$term=="mat.z:cc.z:speciesFRAEXC" |
+                          sumer.orig$term=="elev.z:cc.z:speciesBETPEN" | sumer.orig$term=="elev.z:cc.z:speciesFRAEXC" ),]
 
 
