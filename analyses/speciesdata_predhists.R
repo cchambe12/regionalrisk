16236749297af868a43b93ed4f18b91a2b97e317
cchambe12 <cchamberlain@g.harvard.edu>
2020-03-13 15:30:19 -0400

    working on hists

--- analyses/speciesdata_predhists.R
+++ analyses/speciesdata_predhists.R
@@ -5,6 +5,9 @@
 rm(list=ls()) 
 options(stringsAsFactors = FALSE)
 
+library(RColorBrewer)
+library(ggplot2)
+
 
 ## Set Working Directory
 setwd("~/Documents/git/regionalrisk")
@@ -12,6 +15,36 @@ setwd("~/Documents/git/regionalrisk")
 fs <- read.csv("analyses/output/fs_newspace_orig.csv", header=TRUE)
 fs$spp <- as.numeric(as.factor(fs$species))
 species <- unique(fs$species)
+
+### Plot of histograms
+
+cols <- colorRampPalette(brewer.pal(7,"Accent"))(6)
+
+
+quartz()
+par(mfrow=c(2,2))
+ggplot(fs, aes(x=mst,col=species, fill=species)) + geom_histogram() + coord_cartesian(xlim=c(-7,17)) + 
+  xlab("Mean Spring Temperature (°C)") + 
+  scale_color_manual(values=cols,
+                     labels=c("AESHIP"=expression(paste(italic("Aesculus hippocastanum"))),
+                              "ALNGLU"=expression(paste(italic("Alnus glutinosa"))),
+                              "aaBETPEN"=expression(paste(italic("Betula pendula"))),
+                              "FAGSYL"=expression(paste(italic("Fagus sylvatica"))),
+                              "zFRAEXC"=expression(paste(italic("Fraxinus excelsior"))),
+                              "QUEROB"=expression(paste(italic("Quercus robur"))))) + 
+  scale_fill_manual(values=cols,
+                    labels=c("AESHIP"=expression(paste(italic("Aesculus hippocastanum"))),
+                             "ALNGLU"=expression(paste(italic("Alnus glutinosa"))),
+                             "aaBETPEN"=expression(paste(italic("Betula pendula"))),
+                             "FAGSYL"=expression(paste(italic("Fagus sylvatica"))),
+                             "zFRAEXC"=expression(paste(italic("Fraxinus excelsior"))),
+                             "QUEROB"=expression(paste(italic("Quercus robur")))))
+hist(fs$distkm, xlim=c(0,720), main="", xlab="Distance from Coast (km)")
+hist(fs$elev, xlim=c(0, 1900), main="", xlab="Elevation (m)")
+hist(fs$nao, xlim=c(-0.6, 0.7), main="NAO Index")
+
+
+
 
 ### alright now let's go through the predictors...
 quartz() ## MST
